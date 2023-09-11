46266917667bdbef2adfd1ace773c465633dad9f
shuvojitss <142198651+shuvojitss@users.noreply.github.com>
2023-09-11 12:27:16 +0530

    Create 8pg1.py

---
+++ 8pg1.py
@@ -0,0 +1,8 @@
+dict={'Name':'Zara','Age':7,'Class':'First'}
+print ("dict['Name']:",dict['Name'])
+print("dict['Age']:",dict['Age'])
+print("dict['Class']:",dict['Class'])
+dict['Age']=8;
+dict['School']="DPS School";
+print("dict['Age']:",dict['Age'])
+print("dict['School']:",dict['School'])
