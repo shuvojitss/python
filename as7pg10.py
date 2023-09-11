0f1d0306c3322810b6de1f0d2641febfef42fff5
shuvojitss <142198651+shuvojitss@users.noreply.github.com>
2023-09-11 12:24:25 +0530

    Create as7pg10.py

---
+++ as7pg10.py
@@ -0,0 +1,11 @@
+tuplex=tuple("index tuple")
+print(tuplex)
+index=tuplex.index("p")
+print(index)
+index=tuplex.index("p",5)
+print(index)
+index=tuplex.index("e",3,6)
+print(index)
+#value error exception
+index=tuplex.index("y")
+print(index)
