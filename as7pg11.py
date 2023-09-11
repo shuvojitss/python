4c5c2806151596fa6759672c79a00c323aa8e717
shuvojitss <142198651+shuvojitss@users.noreply.github.com>
2023-09-11 12:24:43 +0530

    Create as7pg11.py

---
+++ as7pg11.py
@@ -0,0 +1,17 @@
+tuplex=(2,3,4,5,4,6,7,8,6,1)
+slice=tuplex[3:5]
+print(slice)
+slice=tuplex[5:]
+print(slice)
+slice=tuple[:]
+print(slice)
+slice=tuplex[-8:-4]
+print(slice)
+tuplex=tuple("HELLO WORLD")
+print(tuplex)
+slice=tuplex[2:9:2]
+print(slice)
+slice=tuplex[:-4]
+slice=tuplex[9:2:-4]
+print(slice)
+
